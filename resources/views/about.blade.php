68d3f8bf35903f0d9c09ed19491b755a47c0ccb0
Mehellice <mehellice@gmail.com>
2021-04-08 23:24:04 +0800

    About
    Page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<div class="about-section">
+  <h1>About Us</h1>
+</div>
+
+<h2 style="text-align:center">some tagline</h2>
+      <div class="container">
+
+        <p>Some text that describes the company lorem ipsum ipsum lorem.Some text that describes the company lorem ipsum ipsum lorem.Some text that describes the company lorem ipsum ipsum lorem.Some text that describes the company lorem ipsum ipsum lorem.Some text that describes the company lorem ipsum ipsum lorem.</p>
+		<br>
+        <p class="aligncenter">
+		<img src="logo.jpg" alt="Logo" class="center" style="width:128px;height:128px;>
+        </p>
+ 	 </div>
+     <hr>
+    <div class="column">
+      <div class="container">
+        <h1>Contact Us</h1>
+        <p class="title">Got any questions? We are here to hep!</p>
+        <form action="/you'll_get_sent_here.php">
+        <div class="card">
+        <div class="container">
+        <br>
+  <label for="email">Email:</label><br>
+  <input type="text" id="email" name="email" value=""><br>
+  <label for="Question">Question:</label><br>
+  <input type="text" id="Question" name="Question" value=""><br><br>
+  <input class="button" type="submit" value="Submit">
+</form> 
+
+
+      </div>
+    </div>
+  </div>
+  
+ 
+
+</body>
+</html>
+
