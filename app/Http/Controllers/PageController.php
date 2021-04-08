c4f3ed87225fd757179af3eb2ff34f0a5553bfb1
aaron li <bransonchen1014@gmail.com>
2021-04-08 21:49:45 +0800

    page controller
    added view function for pages

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PageController extends Controller 
+{
+    public function show($pagename)
+    {
+        return view(pages.{$pagename})
+    }
+
+
+
+
+
+
+
+
+
+
+}
