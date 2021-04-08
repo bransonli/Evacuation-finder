301b3ab3e04bc995c69555f3995ef1d7f832846d
aaron li <bransonchen1014@gmail.com>
2021-04-09 06:47:39 +0800

    slight change on sign up
    reduced header padding

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -6,7 +6,7 @@
 @stop
 @section('content')
 <body>
-    <header class="w3-container w3-red w3-center" style="padding:128px 16px">
+    <header class="w3-container w3-red w3-center" style="padding: 64px;px 16px">
 	
         <h1 class="w3-margin w3-jumbo">SIGN UP</h1>
 	
