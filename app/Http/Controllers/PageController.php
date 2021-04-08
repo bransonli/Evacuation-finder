9de018fb8ebf0ee44c9545255bb8276e47af89e2
aaron li <bransonchen1014@gmail.com>
2021-04-08 22:29:24 +0800

    created function to store to database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -11,6 +11,20 @@ class PageController extends Controller
     }
 
 
+    public function store()
+    {
+        dump(request()->all());
+
+        $user = new users();
+        $user->name = request('name');
+        $user->address = request('address');
+        $user->age = request('age');
+
+        $user->save();
+
+        dd('saved');
+    }
+
 
 
 
