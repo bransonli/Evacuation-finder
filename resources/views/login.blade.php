12279cc5526dfbc47faa3d17124fee7b086b2efb
aaron li <bransonchen1014@gmail.com>
2021-04-08 22:48:13 +0800

    fixed form submission

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,42 +14,43 @@
 	</div>
 
 	<div>
-		<form action="POST" action= "/store_user"></form>
-		@csrf
-		<div class='control'>
-			<label for="name"> Full name</label>
-			<div>
-				<input type="text" name = "name" id="name">
-				<br>
-			</div>
-		</div>
+		<form action="POST" action= "/store_user">
+            @csrf
+            <div>
+                <label for="name"> Full name</label>
+                <div>
+                    <input type="text" name = "name" id="name">
+                    <br>
+                </div>
+            </div>
 
 
-		<div class='control'>
-			<label for="address"> Address</label>
-			<div>
-				<input type="text" name = "address" id="address">
-				<br>
-			</div>
-		</div>
+            <div >
+                <label for="address"> Address</label>
+                <div>
+                    <input type="text" name = "address" id="address">
+                    <br>
+                </div>
+            </div>
 
-		<div class ='control'>
-			<label for="name"> Age</label>
-			<div>
-				<input type="integer" name = "age" id="age">
-				<br>
-			</div>
-		</div>
+            <div >
+                <label for="name"> Age</label>
+                <div>
+                    <input type="integer" name = "age" id="age">
+                    <br>
+                </div>
+            </div>
 
-		<div>
-			<div class="control"> 
-            <br>
-				<button class = "button is-link" type="submit"> submit  </button>
-			</div>
-		
-		
-		
-		</div>
+            <div>
+                <div class="control"> 
+                <br>
+                    <button class = "button is-link" type="submit"> submit  </button>
+                </div>
+            
+            
+            
+            </div>
+        </form>
 
 
 		
