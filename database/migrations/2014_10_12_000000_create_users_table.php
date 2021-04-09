bba57f0561e74c6c7a72981423ea5c35b4ea5e6e
aaron li <bransonchen1014@gmail.com>
2021-04-09 22:48:24 +0800

    reaaranged database for coordinates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('address1');
-            $table->string('address2');
-            $table->string('address3');
-            $table->string('address4');
+            $table->string('latitude');
+            $table->string('longhitude');
             $table->integer('phone');
         });
     }
