c96ffb1789023a4ae0180dbae98a42d7bc53622f
aaron li <bransonchen1014@gmail.com>
2021-04-10 14:08:24 +0800

    added footer to maps

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -112,10 +112,26 @@
     
     <div id="map"></div>
 
+
+    <footer class="w3-container w3-padding-64 w3-center w3-opacity">  
+            <div class="w3-xlarge w3-padding-32">
+                <i class="fa fa-facebook-official w3-hover-opacity"></i>
+                <i class="fa fa-instagram w3-hover-opacity"></i>
+                <i class="fa fa-snapchat w3-hover-opacity"></i>
+                <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+                <i class="fa fa-twitter w3-hover-opacity"></i>
+                <i class="fa fa-linkedin w3-hover-opacity"></i>
+            </div>
+            <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+    </footer>
+
     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
     <script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAslehPIAvH85Xx9imHwVFgX4OOiaN7NjY&callback=initMap&libraries=&v=weekly"
       async
     ></script>
+
+
+
   </body>
 </html>
