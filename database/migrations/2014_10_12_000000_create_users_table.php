9fa1a46dcce0aeb97dce86d3b1dd7f86cb417251
aaron li <bransonchen1014@gmail.com>
2021-04-10 13:47:39 +0800

    updated user data base for first second and last name

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('second_name');
+            $table->string('last_name');
+
             $table->string('latitude');
             $table->string('longhitude');
             $table->string('phone');
