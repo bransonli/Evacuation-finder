39fe8a85ca00f8875747429fdac6c5053fdf47af
aaron li <bransonchen1014@gmail.com>
2021-04-10 14:35:39 +0800

    Update user not found

--- resources/views/user_not_found.blade.php
+++ resources/views/user_not_found.blade.php
@@ -20,22 +20,40 @@
 		<form method='POST' action= "/check_user">
             @csrf
             <div class="w3-row-padding w3-indigo w3-center w3-padding-32 w3-container">
-                <label for="name"><b> Full name</b></label>
+                <label for="name"><b> First name</b></label>
                 <div>
-                    <input type="text" name = "name" id="name">
-                    <div>
-                        <p style="color:red">
-                            User not found 
-                        </p>
-                        
-                        <p style="color:yellow">
-                            NOTE: Please use the full name you used when signing up
-                        </p>
-                    </div>
+                    <input type="text" name = "first_name" id="first_name">
                     <br>
                 </div>
             </div>
 
+            <div class="w3-row-padding w3-indigo w3-center w3-padding-32 w3-container">
+                <label for="name"><b> Second name</b></label>
+                <div>
+                    <input type="text" name = "second_name" id="second_name">
+                    <br>
+                </div>
+            </div>
+
+            <div class="w3-row-padding w3-indigo w3-center w3-padding-32 w3-container">
+                <label for="name"><b> Last name</b></label>
+                <div>
+                    <input type="text" name = "last_name" id="last_name">
+                    <br>
+                </div>
+
+                <div>
+                    <p style="color:red">
+                        User not found 
+                    </p>
+                    
+                    <p style="color:yellow">
+                        NOTE: Please use the names you used when signing up
+                    </p>
+                </div>
+            </div>
+
+          
 
           
             <div class="w3-row-padding w3-indigo w3-center w3-padding-64 w3-container"> 
